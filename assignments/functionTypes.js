929eed55dee892eedf5535800a9f01266ee20a34
deepak joshi <deepak10995@gmail.com>
2019-10-11 03:52:24 +0530

    changes

--- assignments/functionTypes.js
+++ assignments/functionTypes.js
@@ -42,12 +42,24 @@ convertToString(21); // "21"
  * @param {number} n
  * @return {number}
  */
+function convertToString(n) {
+  return String(n + 1);   
+}
+let convertToString1 = function(n) {
+  return String(n + 1);
+};
 
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
+ function convertToString(n) {
+  return String(n - 1);   
+}
+let convertToString1 = function(n) {
+  return String(n-1);
+};
 
 /**
  * Adds two numbers.
@@ -55,6 +67,12 @@ convertToString(21); // "21"
  * @param {number} y
  * @return {number} the sum
  */
+ function convertToString(x,y) {
+  return String(x + y);   
+}
+let convertToString1 = function(x,y) {
+  return String(x*y);
+};
 
 /**
  * Subtracts the second number from the first.
@@ -62,6 +80,12 @@ convertToString(21); // "21"
  * @param {number} y
  * @return {number} the difference
  */
+ function convertToString(x,y) {
+  return String(y - x);   
+}
+let convertToString1 = function(x,y) {
+  return String(y - x);
+};
 
 /**
  * Multiplies two numbers.
@@ -69,7 +93,132 @@ convertToString(21); // "21"
  * @param {number} y
  * @return {number} the product
  */
+ function convertToString(x,y) {
+  return String(x*y);   
+}
+let convertToString1 = function(x,y) {
+  return String(x*y);
+};
 
+/**
+ * Divides the first number by the second.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the quotient
+ */
+ function convertToString(x,y) {
+  return String(x/y);   
+}
+let convertToString1 = function(x,y) {
+  return String(x/y);
+};
+
+/**
+ * Multiplies a number by itself.
+ * @param {number} x, number to be squared
+ * @return {number} squared
+ */
+ function convertToString(x) {
+  return String(x*x);   
+}
+let convertToString1 = function(x) {
+  return String(x*x);
+};
+
+/**
+ * Performs a mathematical operation on two numbers.
+ * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
+ * @param {string} operation "add", "subtract", "multiply", or "divide"
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the result
+ */
+  function convertToString(x,y) { 
+  console.log(` x+y = ${x+y}`);
+}let convertToString1 = function(x,y) {
+  return String(`x+y = ${x+y}`);
+};
+
+/**
+ * Returns true if `a` is greater than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is larger than `b`
+ */
+ function convertToString(a,b) {
+ 	if (a>b) {
+ 		return true
+ 	} else{
+ 		return false
+ 	}
+ 	// body...
+ }
+
+/**
+ * Returns true if `a` is less than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is smaller than `b`
+ */
+ function convertToString(a,b){
+ 	if (a<b){
+ 		return true
+ 	} else{
+ 		return false
+ 	}
+ }
+
+/**
+ * Returns true if `a` and `b` are equal.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} the numbers are equal
+ */
+
+/**
+ * Returns the smallest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the smallest number
+ */
+
+/**
+ * Returns the largest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the largest number
+ */
+
+/**
+ * Returns true if `n` is even.
+ * @param {number} n
+ * @return {boolean} the number is even
+ */
+
+/**
+ * Returns true if `n` is odd.
+ * @param {number} n
+ * @return {boolean} the number is odd
+ */
+
+/**
+ * Returns a letter grade.
+ * "A": 90-100%
+ * "B": 80-89%
+ * "C": 70-79%
+ * "D": 60-69%
+ * "F": 0-59%
+ * @param {number} score
+ * @param {number} total maximum possible score
+ * @return {string} the score represented as a letter grade
+ */
+
+/**
+ * Joins two strings with a space.
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string} joined the words joined with a space
+ */
 /**
  * Divides the first number by the second.
  * @param {number} x
